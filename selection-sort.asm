5619e350f426c9e38b1ba51e391b215083783eb0
CMancillas <carlosmancillas4799@gmail.com>
2025-02-13 14:36:11 -0700

    Finalizacion 'Programacion en Mips' de la tarea 2

--- selection-sort.asm
+++ selection-sort.asm
@@ -1,18 +1,61 @@
-# Algoritmo de selection sort http://en.wikipedia.org/wiki/Selection_sort
-
 	.data
-a:	.word 7, 2, 13, 15, 7, 8, 1, 10
-n:	.word 8
+mensaje: .asciiz "Antes de ordenar, los números son:\n"
+espacio: .asciiz " "
+salto: .asciiz "\n"
+despues: .asciiz "Después de ordenar, los números son:\n"
+error_msg: .asciiz "Error: El tamaño del arreglo debe ser al menos 1.\n"
+a:	.word 5, 3, 8, 6, 2, 7, 4, 10, 1, 9
+n:	.word 10
 
 	.text
+	
+	# Cargar el tamaño del arreglo en $t3
+	lw $t3, n 
+	
+	# Si n < 1, imprimir error y salir
+	blez $t3, error_exit
+	
+	# Cargar la dirección del arreglo en $t5
+	la $t5, a 
+	
+	# Imprimir el mensaje "Antes de ordenar, los números son:"
+	li $v0, 4
+	la $a0, mensaje
+	syscall
+	
+	# Inicializar i en 0
+	li $t0, 0 
+	
+imprimir_arreglo:
+	bge $t0, $t3, fin_impresion # Si i >= n, termina impresión
+	
+	mul $t1, $t0, 4 # Calcular desplazamiento i * 4
+	add $t1, $t5, $t1 # Dirección de a[i]
+	lw $a0, 0($t1) # Cargar a[i] en $a0
+	
+	li $v0, 1 # Syscall para imprimir entero
+	syscall
+	
+	# Imprimir un espacio 
+	li $v0, 4
+	la $a0, espacio
+	syscall
+	
+	addi $t0, $t0, 1 # i++
+	j imprimir_arreglo # Repetir
+	
+fin_impresion: 
+	# Imprimir un salto de línea
+	li $v0, 4
+	la $a0, salto
+	syscall
+	
 	li $t0, 0 # i
 	li $t1, 0 # j
-	li $t2, 0 # iMin
-	lw $t3, n # n
+	li $t2, 0 # iMax
 	addi $t4, $t3, -1 # n - 1
-	la $t5, a  # t5 apunta al arreglo
 	
-forj:	beq $t1, $t4, final2
+forj:	beq $t1, $t4, ordenar_terminado
 	move $t2, $t1
 	addi $t0, $t1, 1
 fori:	beq $t0, $t3, final1
@@ -24,10 +67,10 @@ fori:	beq $t0, $t3, final1
 
 	move $t7, $t2
 	mul $t7, $t7, 4
-	add $t7, $t5, $t7,
-	lw $t7, 0($t7) # a[iMin]
+	add $t7, $t5, $t7
+	lw $t7, 0($t7) # a[iMax]
 	
-	bge $t6, $t7, final3
+	ble $t6, $t7, final3
 	move $t2, $t0
 
 final3: addi $t0, $t0, 1
@@ -36,19 +79,19 @@ final3: addi $t0, $t0, 1
 final1:
 	beq $t2, $t1, final4
 	
-	# obtiene a[j]
+	# Obtiene a[j]
 	move $t6, $t1
 	mul $t6, $t6, 4
 	add $t6, $t5, $t6
 	lw $s0, 0($t6)
 	
-	# obtiene a[iMin]
+	# Obtiene a[iMax]
 	move $t7, $t2
 	mul $t7, $t7, 4
 	add $t7, $t5, $t7
 	lw $s1, 0($t7)
 	
-	# swap
+	# Swap
 	sw $s0, 0($t7)
 	sw $s1, 0($t6)
 
@@ -56,7 +99,49 @@ final4:
 	addi $t1, $t1, 1
 	j forj
 
-final2:
-	# stop
+# Cuando termina el ordenamiento
+ordenar_terminado:
+	# Imprimir el mensaje "Después de ordenar, los números son:"
+	li $v0, 4
+	la $a0, despues
+	syscall
+
+	# Imprimir el arreglo ordenado
+	li $t0, 0	# Inicializar i en 0
+
+imprimir_ordenado:
+	bge $t0, $t3, fin_impresion_ordenado # Si i >= n, termina impresión
+	
+	mul $t1, $t0, 4 # Calcular desplazamiento: i * 4
+	add $t1, $t5, $t1 # Dirección de a[i]
+	lw $a0, 0($t1) # Cargar a[i] en $a0
+	
+	li $v0, 1 # Syscall para imprimir entero
+	syscall
+	
+	# Imprimir un espacio
+	li $v0, 4
+	la $a0, espacio
+	syscall
+	
+	addi $t0, $t0, 1 # i++
+	j imprimir_ordenado # Repetir
+	
+fin_impresion_ordenado:
+	# Imprimir un salto de línea
+	li $v0, 4
+	la $a0, salto
+	syscall
+
+	# Terminar el programa
 	li $v0, 10
+	syscall
+
+# Si el tamaño del arreglo es menor que 1, se imprime un mensaje de error y se sale
+error_exit:
+	li $v0, 4	# Syscall para imprimir string
+	la $a0, error_msg	# Cargar la dirección del mensaje de error
+	syscall
+	
+	li $v0, 10	# Syscall para terminar el programa
 	syscall
